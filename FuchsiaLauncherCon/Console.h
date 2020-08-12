65986a87aea33a91f6a10a27b7818778dfe501a8
Jayson.json <jayson.h@freenet.de>
2020-08-12 14:17:44 +0200

    Update Console.h

--- FuchsiaLauncherCon/Console.h
+++ FuchsiaLauncherCon/Console.h
@@ -1,4 +1,14 @@
 #pragma once
 void debug(std::string text, FHXTIME::Time fakeUseTime) {
-	std::cout << "[" << fakeUseTime.minutes << ":" << fakeUseTime.seconds << "] " << text << std::endl;
+	std::string seconds = "";
+	std::string minutes = "";
+	if (9 > fakeUseTime.seconds) {
+		seconds.push_back('0');
+	}
+	seconds += std::to_string(fakeUseTime.seconds);
+	if (9 > fakeUseTime.minutes) {
+		minutes.push_back('0');
+	}
+	minutes += std::to_string(fakeUseTime.minutes);
+	std::cout << "[" << minutes << ":" << seconds << "] " << text << std::endl;
 }
